0daaf6d4c79b63fb9cf2d71b7736631087139d03
pasindi <pasidingamage@gmail.com>
2024-04-27 20:18:09 +0530

    done

--- src/main/java/lk/ijse/buddiescafe/controller/LoginFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/LoginFromController.java
@@ -3,9 +3,20 @@ package lk.ijse.buddiescafe.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import lk.ijse.buddiescafe.db.DbConnection;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginFromController {
 
@@ -22,8 +33,46 @@ public class LoginFromController {
     private TextField userName;
 
     @FXML
-    void letsSignInOnAction(ActionEvent event) {
+    void letsSignInOnAction (ActionEvent event) throws IOException {
+        String un = userName.getText();
+        String pw = password.getText();
+        try {
+            checkCredential(un, pw);
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "OOPS! something went wrong").show();
+        }
 
+    }
+
+    private void checkCredential(String un, String pw) throws SQLException, IOException {
+        String sql = "SELECT userName, password FROM User WHERE userName = ?";
+
+        Connection connection = DbConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(sql);
+        pstm.setObject(1, un);
+
+        ResultSet resultSet = pstm.executeQuery();
+        if(resultSet.next()) {
+            String dbPw = resultSet.getString(2);
+
+            if(dbPw.equals(pw)) {
+                navigateToTheDashboard();
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Password is incorrect!").show();
+            }
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "user id not found!").show();
+        }
+    }
+
+    private void navigateToTheDashboard() throws IOException {
+        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/dashboard.fxml"));
+
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = (Stage) this.rootNode.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
     }
 
     @FXML
