57a0a996b0dc080e4bd9f3d837fc6d4c91d1a023
pasindi <pasidingamage@gmail.com>
2024-04-27 22:50:17 +0530

    done

--- src/main/java/lk/ijse/buddiescafe/controller/RegisterFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/RegisterFromController.java
@@ -5,11 +5,16 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.buddiescafe.db.DbConnection;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RegisterFromController {
 
@@ -42,7 +47,32 @@ public class RegisterFromController {
 
     @FXML
     void registerOnAction(ActionEvent event) {
+        String eid = employeeID.getText();
+        String uid = userId.getText();
+        String uname = userName.getText();
+        String pw = password.getText();
 
+        saveUser(eid,uid,uname,pw);
+
+    }
+
+    private void saveUser(String eid, String uid, String uname, String pw) {
+        try {
+            String sql = "INSERT INTO User VALUES(?, ?, ?, ?)";
+
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setObject(1, uid);
+            pstm.setObject(2, eid);
+            pstm.setObject(3, uname);
+            pstm.setObject(4, pw);
+
+            if(pstm.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "user saved!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "something happend!").show();
+        }
     }
 
 }
