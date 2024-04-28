1bef1066a055e70f15a08120890b4cd4bd87f40d
pasindi <pasidingamage@gmail.com>
2024-04-28 16:21:52 +0530

    done

--- src/main/java/lk/ijse/buddiescafe/controller/DashboardFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/DashboardFromController.java
@@ -1,4 +1,53 @@
 package lk.ijse.buddiescafe.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class DashboardFromController {
+
+    @FXML
+    void IncomeCostOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void employeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inventoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void maintainsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ordersOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supplierOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userSettingsOnAction(ActionEvent event) {
+
+    }
+
 }
