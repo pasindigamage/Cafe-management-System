2cdb52ff6ab2cde5d677d3ac60dbbf1dd315797e
pasindi <pasidingamage@gmail.com>
2024-05-06 04:29:23 +0530

    done

--- src/main/java/lk/ijse/buddiescafe/controller/MainDashboardFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/MainDashboardFromController.java
@@ -1,4 +1,77 @@
 package lk.ijse.buddiescafe.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainDashboardFromController {
+
+    @FXML
+    private JFXButton employee;
+
+    @FXML
+    private JFXButton inventory;
+
+    @FXML
+    private JFXButton kitchenware;
+
+    @FXML
+    private JFXButton logout;
+
+    @FXML
+    private JFXButton maintain;
+
+    @FXML
+    private JFXButton menu;
+
+    @FXML
+    private JFXButton order;
+
+    @FXML
+    private JFXButton supplier;
+
+    @FXML
+    private Label time;
+
+    @FXML
+    void employeeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inventoryOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kitchenwareOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void maintainOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menuOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supplierOnAction(ActionEvent event) {
+
+    }
+
 }
