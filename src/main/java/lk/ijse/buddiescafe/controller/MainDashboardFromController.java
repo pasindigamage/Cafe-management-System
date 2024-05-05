6aa24e162589153f99046d213d9d87442daf7764
pasindi <pasidingamage@gmail.com>
2024-05-06 05:06:53 +0530

    done

--- src/main/java/lk/ijse/buddiescafe/controller/MainDashboardFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/MainDashboardFromController.java
@@ -7,6 +7,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -47,45 +49,70 @@ public class MainDashboardFromController {
 
     @FXML
     void employeeOnAction(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/employee.fxml"));
-        Parent rootNode = loader.load();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/employee.fxml"));
+        Pane registerPane = fxmlLoader.load();
         subDashboard.getChildren().clear();
-        subDashboard.getChildren().add(rootNode);
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
-    void inventoryOnAction(ActionEvent event) {
-
+    void inventoryOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/inventory.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
-    void kitchenwareOnAction(ActionEvent event) {
-
+    void kitchenwareOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/kitchenWare.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
     void logoutOnAction(ActionEvent event) {
-
+        Stage stage = (Stage) logout.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
-    void maintainOnAction(ActionEvent event) {
-
+    void maintainOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/maintains.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
-    void menuOnAction(ActionEvent event) {
-
+    void menuOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/foodItem.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
-    void orderOnAction(ActionEvent event) {
-
+    void orderOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/order.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
     @FXML
-    void supplierOnAction(ActionEvent event) {
-
+    void supplierOnAction(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/suppliers.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
     }
 
+    public void homeOnAction(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/mainDashboard.fxml"));
+        Pane registerPane = fxmlLoader.load();
+        subDashboard.getChildren().clear();
+        subDashboard.getChildren().add(registerPane);
+    }
 }
--- src/main/java/lk/ijse/buddiescafe/controller/MaintainsFromController.java
+++ src/main/java/lk/ijse/buddiescafe/controller/MaintainsFromController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -26,14 +27,11 @@ public class MaintainsFromController {
 
     @FXML
     void btnBackOnAction(ActionEvent event) throws IOException {
-        AnchorPane rootNode = FXMLLoader.load(this.getClass().getResource("/view/dashboard.fxml"));
-
-        Scene scene = new Scene(rootNode);
-
-        Stage stage = (Stage) this.rootNode.getScene().getWindow();
-        stage.setScene(scene);
-        stage.centerOnScreen();
-    }
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/kitchenWare.fxml"));
+        Pane registerPane = fxmlLoader.load();
+       // subDashboard.getChildren().clear();
+      //  subDashboard.getChildren().add(registerPane);
+        }
 
     @FXML
     void btnOtherMaintainsOnAction(ActionEvent event) throws IOException {
